d61bbcb6ff4d9f1b7a722feb8e2dab4c5d066012
Anonymous Blobster <anonymous@blobster.org>
2016-01-01 00:00:00 +0000

    get rid of perfd for now SW binaries for Xperia AOSP M MR1 v04

--- aosp_qcom/extras/generic/Android.mk
+++ aosp_qcom/extras/generic/Android.mk
@@ -3,34 +3,6 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter rhine shinano kitakami loire, $(PRODUCT_PLATFORM)), )
 
 # ------------------------------------------------------------------------------
-# 32BIT BINS
-# ------------------------------------------------------------------------------
-
-#include $(CLEAR_VARS)
-#LOCAL_MODULE            := perfd
-#LOCAL_MODULE_TAGS       := optional
-#LOCAL_MODULE_SUFFIX     := 
-#LOCAL_MODULE_CLASS      := EXECUTABLES
-#LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/bin
-#LOCAL_MULTILIB          := 32
-#LOCAL_SRC_FILES         := vendor/bin/perfd
-#include $(BUILD_PREBUILT)
-
-# ------------------------------------------------------------------------------
-# 64BIT BINS
-# ------------------------------------------------------------------------------
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := perfd
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := 
-LOCAL_MODULE_CLASS      := EXECUTABLES
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/bin
-LOCAL_MULTILIB          := 64
-LOCAL_SRC_FILES         := vendor/bin64/perfd
-include $(BUILD_PREBUILT)
-
-# ------------------------------------------------------------------------------
 # 32BIT LIBS
 # ------------------------------------------------------------------------------
 
@@ -102,36 +74,6 @@ LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
 LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
 LOCAL_MULTILIB          := 32
 LOCAL_SRC_FILES         := vendor/lib/libaudioalsa.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := libqti-iop
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .so
-LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MULTILIB          := 32
-LOCAL_SRC_FILES         := vendor/lib/libqti-iop.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := libqti-iop-client
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .so
-LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MULTILIB          := 32
-LOCAL_SRC_FILES         := vendor/lib/libqti-iop-client.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := libqti-perfd-client
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_SUFFIX     := .so
-LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MULTILIB          := 32
-LOCAL_SRC_FILES         := vendor/lib/libqti-perfd-client.so
 include $(BUILD_PREBUILT)
 
 # ------------------------------------------------------------------------------
@@ -206,33 +148,6 @@ LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
 LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib64
 LOCAL_MULTILIB          := 64
 LOCAL_SRC_FILES         := vendor/lib64/libaudioalsa.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := libqti-iop
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib64
-LOCAL_MULTILIB          := 64
-LOCAL_SRC_FILES         := vendor/lib64/libqti-iop.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := libqti-iop-client
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib64
-LOCAL_MULTILIB          := 64
-LOCAL_SRC_FILES         := vendor/lib64/libqti-iop-client.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := libqti-perfd-client
-LOCAL_MODULE_TAGS       := optional
-LOCAL_MODULE_CLASS      := SHARED_LIBRARIES
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR)/lib64
-LOCAL_MULTILIB          := 64
-LOCAL_SRC_FILES         := vendor/lib64/libqti-perfd-client.so
 include $(BUILD_PREBUILT)
 
 endif # Kitakami
--- aosp_qcom/extras/proprietary.mk
+++ aosp_qcom/extras/proprietary.mk
@@ -16,19 +16,3 @@ PRODUCT_PACKAGES += \
     Hdmi_cal \
     Headset_cal \
     Speaker_cal
-
-# Perf
-PRODUCT_PACKAGES += \
-    perfd \
-    libqti-iop \
-    libqti-iop-client \
-    libqti-perfd-client \
-
-PRODUCT_PACKAGES += \
-    perf-profile0 \
-    perf-profile1 \
-    perf-profile2 \
-    perf-profile3 \
-    perf-profile4 \
-    perf-profile5 \
-    perf-profile6 \
