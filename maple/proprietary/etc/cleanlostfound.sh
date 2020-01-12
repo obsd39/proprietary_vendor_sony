cbbfd2ab427724a73b680e82a63bf814357b7b85
Christian Oder <myself5@carbonrom.org>
2020-01-12 16:55:43 +0100

    maple: update pinnend blobs

--- maple/proprietary/etc/cleanlostfound.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-# Copyright (c) 2015 Sony Mobile Communications Inc.
-#
-
-/system/bin/rm -r /data/lost+found/.*
-/system/bin/rm -r /cache/lost+found/.*
-/system/bin/rm -r /idd/lost+found/.*
